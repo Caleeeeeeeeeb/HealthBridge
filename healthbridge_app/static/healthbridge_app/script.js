05a22f29f02c09026b1ac4d9d5bd417b1645e913
Caleeeeeeeeeb <geraldezjunjie@gmail.com>
2025-09-24 08:51:13 +0800

    Add HealthBridge project with home, login, register pages and design

---
+++ healthbridge_app/static/healthbridge_app/script.js
@@ -0,0 +1,19 @@
+function validateRegisterForm() {
+    const username = document.forms["registerForm"]["username"].value;
+    const password = document.forms["registerForm"]["password"].value;
+    const errorDiv = document.getElementById("error");
+
+    errorDiv.innerHTML = "";
+
+    if (username.length < 3) {
+        errorDiv.innerHTML = "Username must be at least 3 characters long.";
+        return false;
+    }
+
+    if (password.length < 6) {
+        errorDiv.innerHTML = "Password must be at least 6 characters long.";
+        return false;
+    }
+
+    return true;
+}
