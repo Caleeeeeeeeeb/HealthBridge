bf928c7e7a1e9d3ef5ccfc98298d5fb1d38c279e
Arnel <geraldezjunjie@gmail.com>
2025-09-26 12:13:38 +0800

    eror fix

--- healthbridge_app/views.py
+++ healthbridge_app/views.py
@@ -34,7 +34,7 @@ def login_view(request):
         email = request.POST.get('email')
         password = request.POST.get('password')
 
-        # Authenticate using email as username
+        # since username == email at registration
         user = authenticate(request, username=email, password=password)
         if user is not None:
             login(request, user)
