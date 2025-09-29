e80c22a68f1a7fdfa39d8e1a965916efeca10c0b
rcini19 <gamallobenzleo@gmail.com>
2025-09-29 23:45:40 +0800

    Updated the UI and removed redundant buttons, this is to be continued

--- healthbridge_app/static/healthbridge_app/script.js
+++ healthbridge_app/static/healthbridge_app/script.js
@@ -1,6 +1,7 @@
 function validateRegisterForm() {
     const username = document.forms["registerForm"]["username"].value;
-    const password = document.forms["registerForm"]["password"].value;
+    const password = document.forms["registerForm"]["password1"].value;
+    const confirmPassword = document.forms["registerForm"]["password2"].value;
     const errorDiv = document.getElementById("error");
 
     errorDiv.innerHTML = "";
@@ -12,6 +13,11 @@ function validateRegisterForm() {
 
     if (password.length < 6) {
         errorDiv.innerHTML = "Password must be at least 6 characters long.";
+        return false;
+    }
+
+    if (password !== confirmPassword) {
+        errorDiv.innerHTML = "Passwords do not match.";
         return false;
     }
 
